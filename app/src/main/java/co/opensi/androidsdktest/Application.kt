5e049a3df9ffb03c9516b5e5cad550beef155c9a
Shegun MONTCHO <shekmontcho@gmail.com>
2024-07-15 14:52:37 +0100

    remove api_key

--- app/src/main/java/co/opensi/androidsdktest/Application.kt
+++ app/src/main/java/co/opensi/androidsdktest/Application.kt
@@ -9,7 +9,7 @@ class Application : Application() {
         super.onCreate()
         //Initialisation de l'API
         Kkiapay.init(applicationContext,
-                "LprYUAyMpfAjq4z2yTHPiY0b6XktIQ",
+                "",
             SdkConfig(themeColor = R.color.colorPrimary,
                     /* set enableSandbox = false in case you are using live API Keys */
                     enableSandbox = false )
